aa4cf187be3d3b885f29e16588703450560ce013
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2024-10-12 15:34:22 +0200

    task 3 is done

---
+++ task_02/task_02.py
@@ -0,0 +1,29 @@
+def bi_search(arr, target):
+    left, right = 0, len(arr) - 1
+    iterations = 0
+    upper = None
+
+    while left <= right:
+        iterations +=1
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            upper = arr[mid]
+            return (iterations, upper)
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            upper = arr[mid]
+            right = mid - 1
+
+    if left < len(arr):
+        upper = arr[left]
+
+    return (iterations, upper)
+
+# Тестуємо функцію:
+arr = [0.1, 0.5, 1.3, 2.7, 3.6, 5.8, 7.9, 9.1]
+target = 3.6
+
+result = bi_search(arr, target)
+print(result) 
